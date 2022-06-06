e4719c3c63afecb6e4791474b390d8923a97a382
harold alzate <haroldalzate99@gmail.com>
2022-06-06 15:01:10 -0500

    guardando avances

--- src/componentes/CartWidget.jsx
+++ src/componentes/CartWidget.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart'
 
 
 function CartWidget({numero}) {
     return ( 
     <div >
-        <AddShoppingCartIcon />
+        <ion-icon name="cart-outline"></ion-icon>
         
-        {numero}
+   
+         {numero}
          </div>
         
     );
