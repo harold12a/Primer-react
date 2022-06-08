90d72f8f1f5369ba50bbb1439c32593d29ed840b
harold alzate <haroldalzate99@gmail.com>
2022-06-08 12:36:42 -0500

    agregando un solo personaje api

--- src/componentes/ItemDetailCOntainer.jsx
+++ src/componentes/ItemDetailCOntainer.jsx
@@ -28,38 +28,3 @@ function ItemDetailCOntainer() {
 export default ItemDetailCOntainer;
 
 
-
-
-{/*import React,{useEffect, useState} from 'react';
-import ItemDetail from './ItemDetail';
-
-
-
-function ItemDetailCOntainer() {
-    const[characters,setCharacters]=useState([]);
-    const initialUrl ="https://rickandmortyapi.com/api/character";
-   
-    const fetchCharacters = (url)=>{
-        fetch(url)
-        .then((Response)=>Response.json())
-        .then((data)=>setCharacters(data.results))
-        .catch(error=>console.log(error))
-     };
-     useEffect(()=>{
-         fetchCharacters(initialUrl);
-     },[])
-
-    return ( 
-       <div className='container mt-5'>
-       <ItemDetail characters={characters}/>
-        
-    </div>
-      
-  
-         );}
-   
- 
-
-
-    export default ItemDetailCOntainer;*/}
-
